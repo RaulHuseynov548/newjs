6c8afe510889fc436aee231ea2dd5768409ab4b2
RaulHuseynov548 <raulhuseynov548@gmail.com>
2024-02-08 14:12:18 +0400

    second comit

---
+++ between.js
@@ -0,0 +1,17 @@
+
+
+var a = 1;
+var b = 4;
+
+
+function uzunluq (a,b) {
+    netice = []
+    for (var i = a; i <= b; i++ ){
+
+        netice.push(i)
+    }
+    return netice
+
+
+}
+console.log(uzunluq(a,b))
