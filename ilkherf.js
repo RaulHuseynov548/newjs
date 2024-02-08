72d8ef526280df26b7f5391d8c72713f0ef81746
RaulHuseynov548 <raulhuseynov548@gmail.com>
2024-02-08 13:39:50 +0400

    second comit

---
+++ ilkherf.js
@@ -0,0 +1,21 @@
+
+var adlar =["raul","zayl","Rauf","Zaul"];
+var netice = [];
+var ilkherf
+
+function boyukherf (adlar){
+    for (var i = 0; i<adlar.length; i++ ) {
+
+        ilkherf = adlar[i][0].toUpperCase() + adlar[i].slice(1)
+
+       console.log(ilkherf);
+        
+       
+
+
+        
+    }
+    return (netice)
+   
+}
+console.log(boyukherf(adlar))
